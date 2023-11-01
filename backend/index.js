1194414d799fd92fb6479e5bf8ed8c7f2ede203d
psbharadwaj2002 <psbharadwaj.scsvmv@gmail.com>
2023-11-01 22:37:18 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mongoDB = require("../backend/db");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
