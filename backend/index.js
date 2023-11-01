6f5fa571112f3793c53789323d8e82567550e14f
psbharadwaj2002 <psbharadwaj.scsvmv@gmail.com>
2023-11-01 22:27:35 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,31 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
-app.use((request, response, next) => {
-  response.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://steady-pothos-ab81dd.netlify.app, http://localhost:3000"
-  );
-  response.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((request, response, next) => {
+//   response.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://steady-pothos-ab81dd.netlify.app, http://localhost:3000"
+//   );
+//   response.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use(
+  cors({
+    origin: [
+      "https://steady-pothos-ab81dd.netlify.app",
+      "http://localhost:3000",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   console.log("Success");
