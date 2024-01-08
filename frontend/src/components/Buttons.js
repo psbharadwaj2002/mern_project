ae5bc5b6ac4f84003b5ec353269cb628300164eb
psbharadwaj2002 <psbharadwaj.scsvmv@gmail.com>
2024-01-08 16:48:55 +0530

    button carousel

--- frontend/src/components/Buttons.js
+++ frontend/src/components/Buttons.js
@@ -83,30 +83,7 @@ function Buttons() {
             <img src={mobileCarousel} className="d-block w-100" alt="..." />
           </div>
         </div>
-        {/* <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button> */}
+        {/* BUTTON BELOW */}
       </div>
 
       {/***********************************************************  BUTTONS ********************************************************/}
@@ -209,3 +186,30 @@ function Buttons() {
 }
 
 export default Buttons;
+
+{
+  /* <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button> */
+}
